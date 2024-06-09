7dbf8c61c05b2020ef85d8b53241391d1f7c93ca
Богдан <kiselev.bogdan03@mail.ru>
2024-06-09 12:42:41 +0300

    RSA.js
    Шифрование и расшифрование на JS

--- main.js
+++ main.js
@@ -25,6 +25,21 @@ function decrypt(encryptedMessage, n, d) {
     return (encryptedMessage ** d) % n;
 }
 
+// Алфавит
+const alphabet = {
+    'а': 1, 'б': 2, 'в': 3, 'г': 4, 'д': 5,
+    'е': 6, 'ё': 7, 'ж': 8, 'з': 9, 'и': 10,
+    'й': 11, 'к': 12, 'л': 13, 'м': 14, 'н': 15,
+    'о': 16, 'п': 17, 'р': 18, 'с': 19, 'т': 20,
+    'у': 21, 'ф': 22, 'х': 23, 'ц': 24, 'ч': 25,
+    'ш': 26, 'щ': 27, 'ъ': 28, 'ы': 29, 'ь': 30,
+    'э': 31, 'ю': 32, 'я': 33
+};
+const reverseAlphabet = Object.entries(alphabet).reduce((acc, [key, value]) => {
+    acc[value] = key;
+    return acc;
+}, {});
+
 // Параметры RSA
 const p = 3;
 const q = 11;
@@ -39,6 +54,10 @@ function userB(message) {
     const encryptedMessage = message.map(m => encrypt(m, n, e));
     console.log("Encrypted message:", encryptedMessage);
 
+    // Переводим зашифрованные числа в буквы для демонстрации
+    const encryptedText = encryptedMessage.map(m => reverseAlphabet[m] || '?').join('');
+    console.log("Encrypted text:", encryptedText);
+
     // Отправляем сообщение пользователю А
     return encryptedMessage;
 }
@@ -50,14 +69,14 @@ function userA(encryptedMessage) {
     console.log("Decrypted message:", decryptedMessage);
 
     // Переводим расшифрованные числа в буквы
-    const originalMessage = decryptedMessage.map(m => String.fromCharCode(m));
+    const originalMessage = decryptedMessage.map(m => reverseAlphabet[m]);
     console.log("Original message:", originalMessage.join(""));
     return decryptedMessage;
 }
 
 // Вводим свое сообщение
-const yourMessage = "watch";
-const messageCodes = yourMessage.split("").map(char => char.charCodeAt(0));
+const yourMessage = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+const messageCodes = yourMessage.split("").map(char => alphabet[char]);
 
 // Выполняем пример работы пользователей
 const encryptedMessage = userB(messageCodes);
